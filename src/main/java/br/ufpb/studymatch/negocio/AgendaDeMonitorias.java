2b4d733dfcea097f769a510337f460fa4063da30
FabricioOliveira08 <Fernandesfabricio2019@gmail>
2026-07-23 18:13:42 -0300

    implementa a interface do sistema  e o gerenciamento de dados

--- src/main/java/br/ufpb/studymatch/negocio/AgendaDeMonitorias.java
+++ src/main/java/br/ufpb/studymatch/negocio/AgendaDeMonitorias.java
@@ -1,4 +1,89 @@
 package br.ufpb.studymatch.negocio;
 
-public class AgendaDeMonitorias {
+import br.ufpb.studymatch.entidades.Aluno;
+import br.ufpb.studymatch.entidades.SessaoEstudo;
+import br.ufpb.studymatch.excecoes.SessaoJaExisteException;
+import br.ufpb.studymatch.excecoes.SessaoNaoEncontradaException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AgendaDeMonitorias implements ISistemaMonitoria{
+
+    private Map<String, SessaoEstudo> sessoes;
+
+    public AgendaDeMonitorias() {
+        this.sessoes = new HashMap<>();
+    }
+
+    @Override
+    public void cadastrarSessao(SessaoEstudo sessao) throws SessaoJaExisteException {
+        if(this.sessoes.containsKey(sessao.getId())) {
+            throw new SessaoJaExisteException("Já existe uma sessão cadastrada com o ID: " + sessao.getId());
+        }
+        this.sessoes.put(sessao.getId(), sessao);
+    }
+
+    @Override
+    public void removerSessao(String id) throws SessaoNaoEncontradaException {
+        if(!this.sessoes.containsKey(id)) {
+            throw new SessaoNaoEncontradaException("Não foi possível remover. Sessão não encontrada para o ID: " + id);
+        }
+        this.sessoes.remove(id);
+    }
+
+    @Override
+    public SessaoEstudo pesquisarSessaoPorId(String id) throws SessaoNaoEncontradaException {
+        if(!this.sessoes.containsKey(id)) {
+            throw new SessaoNaoEncontradaException("Sessão não encontrada com o ID: " + id);
+        }
+        return this.sessoes.get(id);
+    }
+
+    @Override
+    public void atualizarSessao(String id, SessaoEstudo novaSessao) throws SessaoNaoEncontradaException {
+        if(!this.sessoes.containsKey(id)) {
+            throw new SessaoNaoEncontradaException("Sessão não encontrada com o ID: " + id);
+        }
+        this.sessoes.put(id, novaSessao);
+    }
+
+    @Override
+    public List<SessaoEstudo> listarTodasSessoes() {
+        return new ArrayList<>(this.sessoes.values());
+    }
+
+    @Override
+    public List<SessaoEstudo> listarSessoesPorDisciplina(String disciplina) {
+        return this.sessoes.values().stream()
+                .filter(sessao -> sessao.getDisciplina().equalsIgnoreCase(disciplina))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> obterNomesDosMonitores() {
+        return this.sessoes.values().stream()
+                .map(SessaoEstudo::getNomeMonitor)
+                .distinct().collect(Collectors.toList());
+    }
+
+    @Override
+    public void cadastrarAlunoEmSessao(String idSessao, Aluno aluno) throws SessaoJaExisteException, SessaoNaoEncontradaException {
+        SessaoEstudo sessao = pesquisarSessaoPorId(idSessao);
+        sessao.adicionarAluno(aluno);
+    }
+
+    @Override
+    public void salvarDados() throws IOException {
+
+    }
+
+    @Override
+    public void recuperarDados() throws IOException {
+
+    }
 }
--- src/main/java/br/ufpb/studymatch/negocio/ISistemaMonitoria.java
+++ src/main/java/br/ufpb/studymatch/negocio/ISistemaMonitoria.java
@@ -1,4 +1,22 @@
 package br.ufpb.studymatch.negocio;
 
-public class ISistemaMonitoria {
+import br.ufpb.studymatch.entidades.Aluno;
+import br.ufpb.studymatch.entidades.SessaoEstudo;
+import br.ufpb.studymatch.excecoes.SessaoNaoEncontradaException;
+import br.ufpb.studymatch.excecoes.SessaoJaExisteException;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ISistemaMonitoria {
+    void cadastrarSessao(SessaoEstudo sessao) throws SessaoJaExisteException;
+    void removerSessao(String id) throws SessaoNaoEncontradaException;
+    SessaoEstudo pesquisarSessaoPorId(String id) throws SessaoNaoEncontradaException;
+    void atualizarSessao(String id, SessaoEstudo novaSessao) throws SessaoNaoEncontradaException;
+    List<SessaoEstudo> listarTodasSessoes();
+    List<SessaoEstudo> listarSessoesPorDisciplina(String disciplina);
+    List<String> obterNomesDosMonitores();
+    void cadastrarAlunoEmSessao(String idSessao, Aluno aluno) throws SessaoJaExisteException, SessaoNaoEncontradaException;
+    void salvarDados() throws IOException;
+    void recuperarDados() throws IOException;
 }
