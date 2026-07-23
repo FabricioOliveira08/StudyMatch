76703ca229852bc0a9a8cee209fc135571da1900
FabricioOliveira08 <Fernandesfabricio2019@gmail>
2026-07-22 23:10:52 -0300

    implementa as entidades

--- src/main/java/br/ufpb/studymatch/entidades/Aluno.java
+++ src/main/java/br/ufpb/studymatch/entidades/Aluno.java
@@ -1,4 +1,31 @@
 package br.ufpb.studymatch.entidades;
 
-public class Aluno {
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String matricula;
+    private String nome;
+    private String curso;
+
+    public Aluno(String matricula, String nome, String curso) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.curso = curso;
+    }
+
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCurso() { return curso; }
+    public void setCurso(String curso) { this.curso = curso; }
+
+    @Override
+    public String toString() {
+        return nome + " (" + matricula + ") - " + curso;
+    }
 }
--- src/main/java/br/ufpb/studymatch/entidades/SessaoEstudo.java
+++ src/main/java/br/ufpb/studymatch/entidades/SessaoEstudo.java
@@ -1,4 +1,48 @@
 package br.ufpb.studymatch.entidades;
 
-public class SessaoEstudo {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessaoEstudo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String disciplina;
+    private String nomeMonitor;
+    private List<Aluno> alunosInscritos;
+
+    public SessaoEstudo(String id, String disciplina, String nomeMonitor) {
+        this.id = id;
+        this.disciplina = disciplina;
+        this.nomeMonitor = nomeMonitor;
+        this.alunosInscritos = new ArrayList<>();
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        this.alunosInscritos.add(aluno);
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public String getNomeMonitor() {
+        return nomeMonitor;
+    }
+    public void setNomeMonitor(String nomeMonitor) {
+        this.nomeMonitor = nomeMonitor;
+    }
+
+    public List<Aluno> getAlunosInscritos() { return alunosInscritos; }
 }
